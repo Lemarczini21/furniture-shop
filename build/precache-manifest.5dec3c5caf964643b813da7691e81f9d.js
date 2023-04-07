3eaca2b4ad3ce4794e17604ab6f77537cbfbd6e2
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-04-07 07:25:07 +0200

    Remove build from gitignore

---
+++ build/precache-manifest.5dec3c5caf964643b813da7691e81f9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90168425b0aeecb962f166c1de949c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855b90b1c91836470d33",
+    "url": "/static/css/2.dc660faf.chunk.css"
+  },
+  {
+    "revision": "4aeae283496cadd88db3",
+    "url": "/static/css/main.64b4c999.chunk.css"
+  },
+  {
+    "revision": "855b90b1c91836470d33",
+    "url": "/static/js/2.a73aefe0.chunk.js"
+  },
+  {
+    "revision": "4aeae283496cadd88db3",
+    "url": "/static/js/main.4bd31bf7.chunk.js"
+  },
+  {
+    "revision": "3d5b1b60935895d7db2c",
+    "url": "/static/js/runtime-main.efc0c886.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
